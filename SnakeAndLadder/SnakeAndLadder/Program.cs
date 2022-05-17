c759e07d107d8075da91f99726e6aca36c3d0a9c
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-17 22:35:35 +0530

    [Madhavi] added function is startgame

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SnakeLadderClass snakeladder = new SnakeLadderClass();
+            snakeladder.startGame();
+            
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/SnakeLadderClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeLadderClass
+    {
+        int startPosition = 0;
+        public void startGame()
+        {
+            Console.WriteLine("Start playing snake and ladder game");
+            Console.WriteLine("Start position " + startPosition);
+        }
+
+    }
+}
