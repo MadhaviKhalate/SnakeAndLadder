ec8712347f6caddab2c97eeb38c78f65c1ddd054
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-17 22:58:20 +0530

    [Madhavi] check status is NoPlay ladder or snake

--- SnakeAndLadder/SnakeAndLadder/SnakeLadderClass.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeLadderClass.cs
@@ -9,13 +9,29 @@ namespace SnakeAndLadder
     internal class SnakeLadderClass
     {
         Random rand = new Random();
-        int startPosition = 0, number;
+        int startPosition = 0, number, check, LADDER = 1, SNAKE = 2;
         public void startGame()
         {
             number = rand.Next(1, 7);
+            check = rand.Next(0, 3);
             Console.WriteLine("Start playing snake and ladder game");
             Console.WriteLine("Start position " + startPosition);
             Console.WriteLine("Player get number " + number);
+         
+            if (check == LADDER)
+            {
+                startPosition += number;
+                Console.WriteLine("Moves ahead by " + number);
+                Console.WriteLine("Current position of player " + startPosition);
+            }
+            else if (check == SNAKE)
+            {
+                startPosition -= number;
+                Console.WriteLine("Moves behind by " + number);
+                Console.WriteLine("Current position of player " + startPosition);
+            }
+            else
+                Console.WriteLine("NoPlay!!!!!  player stay in same position");
         }
 
     }
