9aa84efccf20b3dc9d37c0282212e2f377b6188d
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-18 22:45:49 +0530

    [Madhavi] add some code

--- SnakeAndLadder/SnakeAndLadder/SnakeLadderClass.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeLadderClass.cs
@@ -9,14 +9,14 @@ namespace SnakeAndLadder
     internal class SnakeLadderClass
     {
         Random rand = new Random();
-        int startPosition, number, check, diceRollCount = 0, NO_PLAY=0, 
+        int playerPosition, number, updatePosition, check, diceRollCount = 0, NO_PLAY=0, 
             LADDER = 1, SNAKE = 2, END_WIN_POSITION = 100;
         public void startGame()
         {
             Console.WriteLine("Start playing snake and ladder game");
-            Console.WriteLine("Start position " + startPosition);
+            Console.WriteLine("Start position " + playerPosition);
             
-            for (startPosition = 0; startPosition < END_WIN_POSITION;)
+            for (playerPosition = 0; playerPosition < END_WIN_POSITION;)
             {
                 check = rand.Next(0, 3);
                 number = rand.Next(1, 7);
@@ -24,33 +24,34 @@ namespace SnakeAndLadder
 
                 if (check == LADDER)
                 {
-                    startPosition += number;
+                    playerPosition += number;
                     Console.WriteLine("Moves ahead by " + number);
                 }
                 else if (check == SNAKE)
                 {
-                    startPosition -= number;
+                    playerPosition -= number;
                     Console.WriteLine("Moves behind by " + number);
-                    if (startPosition < 0)
+                    if (playerPosition < 0)
                     {
-                        startPosition = 0;
+                        playerPosition = 0;
                     }
-
                 }
                 else
                 {
-                    startPosition += NO_PLAY;
+                    playerPosition += NO_PLAY;
                 }
                 diceRollCount++;
                 Console.WriteLine("position after every die roll " + diceRollCount);
             }
-            Console.WriteLine("Current position of player " + startPosition + "\n");
-            if (startPosition > 100)
-            {
-                startPosition -= number;
-            }
+                if (playerPosition > 100)
+                {
+                    playerPosition -= number;
+                    updatePosition = 100 - playerPosition;
+                    playerPosition += updatePosition;
+                    
+                }
+            Console.WriteLine("\n Current position of player " + playerPosition);
             Console.WriteLine(diceRollCount + " number of times dice was rolled");
         }
-
     }
 }
